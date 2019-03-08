488c616a497cb0f65156fe5092038e28aab02306
Getu Gebre <getugebre@getus-macbook-air.local>
2019-03-07 18:39:29 -0600

    Initial Commit

---
+++ BDayGuess/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BDayGuess
+//
+//  Created by Getu Gebre on 3/7/19.
+//  Copyright © 2019 Getu Gebre. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
